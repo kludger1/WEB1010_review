149bd327770559d736d76204a6f682bba56a128d
kludger1 <kludger@techlaunch.io>
2018-07-02 21:25:04 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// var num = 5;
+// var strings =  "I really appreciate " + "the watermelon"+ 
+//              "its very relaxing" +"I could eat this "+"forever, you dont even understand";
+// var num1 = 1
+// var num2 = 2
+// var num3 = 3
+// var num4 = 4
+// var num5 = 5
+// console.log('${num1 + num2 + num3 + num4 + num5}')
+// var array = [4][3][2][1]["chicken"]["potato"][4>6][4=4][10>3][p,body,style]
+// object = 4,"pop",[5][10][30], age= 23;
+var h2 = document.getElementById('h2')
+var p = document.getElementsByClassName('.food')
+var ut = document.getElementsById('utilities')
+var input= doc.findElementByAttribute()
